2b521f3f19354a37e20be24e34a08f8e7c7c43d8
Anant <anant.asty@gmail.com>
2014-04-26 11:30:20 -0600

    Invite form is almost done

--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class ChatRoom(models.Model):
@@ -11,3 +12,6 @@ class ChatRoom(models.Model):
 
 class ChatUser(User):
     user_intro = models.CharField(max_length=50)
+    score = models.FloatField(validators=[MinValueValidator(1),
+                                          MaxValueValidator(4)])
+    referral_user = models.ForeignKey("ChatUser", null=True, blank=True)
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,18 @@
+import hashlib
+
+import rethinkdb as rdb
+
+from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import FormView
+from django.core.mail import send_mail
 
+from chat import mixins
 from chat.models import ChatRoom
-from chat.forms import ChatUserForm
+from chat.forms import ChatUserForm, ChatUserInviteForm
+
+rdb.connect('localhost',28015, db='chat').repl()
 
 @login_required
 def index(request):
@@ -13,9 +22,19 @@ def index(request):
     }
     return render(request,'chats/index.html', context)
 
+def generate_user_hash(chat, user):
+    md5_hash = hashlib.md5()
+    md5_hash.update('{}{}'.format(chat, user))
+    return md5_hash.hexdigest()
+
+def register_hash(user, md5_hash):
+    pass
+
 @login_required
 def chat_room(request, chat_room_id):
     chat = get_object_or_404(ChatRoom, pk=chat_room_id)
+    md5_hash = generate_user_hash(chat, request.user)
+    register_hash(md5_hash)
     return render(request, 'chats/chat_room.html', {'chat': chat,
                                                     'user': request.user})
 
@@ -31,8 +50,25 @@ class RegisterationView(FormView):
     success_url = 'chats/thanks/'
 
     def form_valid(self, form):
-        user = form.save()
-        print user
+        user = form.save(commit=False)
+        user.score = 1
+        user.save()
+        return super(RegisterationView, self).form_valid(form)
+
+
+class UserInvitationView(mixins.LoginRequiredMixin, RegisterationView):
+    form_class = ChatUserInviteForm
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.referral_user = self.request.user
+        user.save()
+        subject = '{}! Invitation to sign up for hoops.com'.format(
+            user.first_name)
+        message = "{} {} You have been invited to sign up for..".format(
+            'SITE_URL')
+        from_email = user.referral_user.email or settings.DEFAULT_FROM_EMAIL
+        send_mail(subject, message, from_email, [user.email])
         return super(RegisterationView, self).form_valid(form)
 
 
